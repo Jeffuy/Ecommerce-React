72bb13454e2edb16827090441f5a512140faf9d2
Jeff Free <jefffreelance3@gmail.com>
2021-11-27 21:31:35 -0300

    :rocket: fixing deploy problem

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '@containers/Layout';
-import Home from '@pages/Home';
-import Login from '@pages/Login';
-import PasswordRecovery from '@pages/PasswordRecovery';
-import SendEmail from '@pages/SendEmail';
-import NewPassword from '@pages/NewPassword';
-import MyAccount from '@pages/MyAccount';
-import CreateAccount from '@pages/CreateAccount';
-import Checkout from '@pages/Checkout';
-import Orders from '@pages/Orders';
-import NotFound from '@pages/NotFound';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "@containers/Layout";
+import Home from "@pages/Home";
+import Login from "@pages/Login";
+import PasswordRecovery from "@pages/PasswordRecovery";
+import SendEmail from "@pages/SendEmail";
+import NewPassword from "@pages/NewPassword";
+import MyAccount from "@pages/MyAccount";
+import CreateAccount from "@pages/CreateAccount";
+import Checkout from "@pages/Checkout";
+import Orders from "@pages/Orders";
+import NotFound from "@pages/NotFound";
 import AppContext from "@context/AppContext";
-import useInitialState from '@hooks/useInitialState';
+import useInitialState from "@hooks/useInitialState";
 
-import '@styles/global.css';
+import "@styles/global.css";
 
 const App = () => {
     const initialState = useInitialState();
@@ -24,18 +24,17 @@ const App = () => {
                 <Layout>
                     <Routes>
                         <Route exact path="/" element={<Home />} />
-                        <Route exact path="/login" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
                         <Route
-                            exact
                             path="/password-recovery"
                             element={<PasswordRecovery />}
                         />
-                        <Route exact path="/send-email" element={<SendEmail />} />
-                        <Route exact path="/new-password" element={<NewPassword />} />
-                        <Route exact path="/account" element={<MyAccount />} />
-                        <Route exact path="/signup" element={<CreateAccount />} />
-                        <Route exact path="/checkout" element={<Checkout />} />
-                        <Route exact path="/orders" element={<Orders />} />
+                        <Route path="/send-email" element={<SendEmail />} />
+                        <Route path="/new-password" element={<NewPassword />} />
+                        <Route path="/account" element={<MyAccount />} />
+                        <Route path="/signup" element={<CreateAccount />} />
+                        <Route path="/checkout" element={<Checkout />} />
+                        <Route path="/orders" element={<Orders />} />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </Layout>
